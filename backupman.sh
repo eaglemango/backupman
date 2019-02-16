3fc0f387e1d962e391119262e78309647d3ba0e2
Sergey Gorbunov <eaglemango@gmail.com>
2019-02-17 00:50:16 +0300

    Add basic functionality

--- backupman.sh
+++ backupman.sh
@@ -1,3 +1,24 @@
-#! /bin/bash
+#!/bin/bash
+
+if [ ! -d $HOME/backupman-archives ]; then
+  mkdir $HOME/backupman-archives
+fi
+
+cd $HOME/backupman-archives
+
+DIR_NAME=$1
+ARCHIVE_NAME=$2
+
+mkdir $DIR_NAME
+
+shopt -s globstar
+for file in $HOME/**/*.cpp; do
+  cp $file $DIR_NAME
+done
+
+tar -cf $ARCHIVE_NAME.tar $DIR_NAME
+
+rm -rf $DIR_NAME
 
 echo "done"
+
