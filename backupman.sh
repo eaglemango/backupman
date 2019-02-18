6f212a3e9acd39ebe0314f764813167c3be1c6ee
Sergey Gorbunov <eaglemango@gmail.com>
2019-02-18 03:19:43 +0300

    Fix whitespaces interpretation

--- backupman.sh
+++ backupman.sh
@@ -11,20 +11,20 @@ ARCHIVE_NAME=$2
 shift
 shift
 
-mkdir $DIR_NAME
+mkdir "$DIR_NAME"
 
 shopt -s globstar
 for file_type in $@; do
   for file in $HOME/**/*.$file_type; do
-    if [[ ( -r $file ) && ( -f $file ) ]]; then
-      cp $file $DIR_NAME
+    if [[ ( -r "$file" ) && ( -f "$file" ) ]]; then
+      cp "$file" "$DIR_NAME"
     fi
   done
 done
 
 DATE=`date +%d.%m.%Y-%H:%M:%S`
-tar --force-local -cf "$ARCHIVE_NAME-$DATE".tar $DIR_NAME
+tar --force-local -cf "$ARCHIVE_NAME-$DATE".tar "$DIR_NAME"
 
-rm -rf $DIR_NAME
+rm -rf "$DIR_NAME"
 
 echo "done"
